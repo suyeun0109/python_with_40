18c74bd05a775a58d70d73557c460cdbb9d1891d
suyeun0109 <102908528+suyeun0109@users.noreply.github.com>
2024-05-20 16:11:30 +0900

    9

--- 9. 영어로된 문서를 한글로 자동번역/main9-3.py
+++ 9. 영어로된 문서를 한글로 자동번역/main9-3.py
@@ -2,7 +2,8 @@ from os import linesep
 import googletrans
 translator=googletrans.Translator()
 
-read_file_path=r"C:\Users\suyeu\Desktop\파이썬과 40개의 작품들\9. 영어로된 문서를 한글로 자동번역\영어파일.txt"
+read_file_path=r"C:\Users\suyeu\Desktop\파이썬과 40개의 작품들\python_with_40\9. 영어로된 문서를 한글로 자동번역\영어파일.txt"
+write_file_path= r"C:\Users\suyeu\Desktop\파이썬과 40개의 작품들\python_with_40\9. 영어로된 문서를 한글로 자동번역\번역파일.txt"
 
 with open(read_file_path,'r') as f:
     readLines=f.readlines()
@@ -10,3 +11,5 @@ with open(read_file_path,'r') as f:
 for lines in readLines:
     result1=translator.translate(lines,dest='ko')
     print(result1.text)
+    with open(write_file_path,'a',encoding='UTF8') as f:
+        f.write(result1.text+'\n')
